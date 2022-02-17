e15f8e077d81c0420f5a00c21c58805dc442aa08
1T6 <qpdlf8@gmail.com>
2022-02-17 10:18:41 +0900

    asdf

--- 알고리즘문제해결전략/clocksync.cpp
+++ 알고리즘문제해결전략/clocksync.cpp
@@ -16,3 +16,62 @@ int switches[9][5] ={
     {3,14,15},
     {1,2,3,4,5},
     {3,4,5,9,13}};
+
+bool map[9][16];
+
+int main()
+{  
+    mapping();
+
+    int numCases;
+    vector<int> results;
+    cin>>numCases;
+
+    for(int i=0; i<numCases; i++){
+        int clocks[16]={0};
+        for(int j=0; j<16; j++){
+            cin>>clocks[j];
+        }
+
+        results.push_back(countNum);
+    }
+
+    for(int i=0; i<16; i++){
+        cout<<results[i];
+    }
+
+
+    return 0;
+}
+
+void mapping()
+{
+
+    for(int i=0; i<9; i++){
+        int least = switches[i][0];
+        for(int j=0; j<5; j++){
+            if(switches[i][j]<least){
+                break;
+            }
+            else{
+                map[i][switches[i][j]]=true;
+            }
+        }
+    }
+
+
+}
+
+int countNum(int[] clocks)
+{
+    int target=0;
+    for(int i=0; i<16; i++){
+        if(clocks[i]%12 != 0){
+            target = i;
+        }
+    }
+
+    int ret;
+
+
+}
