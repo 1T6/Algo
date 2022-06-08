85dc1bf6b0c64c586484f05d3326fe80d7eb0661
1T6 <qpdlf8@gmail.com>
2022-06-08 19:58:30 +0900

    asdf

--- 알고리즘문제해결전략/picnic.cpp
+++ 알고리즘문제해결전략/picnic.cpp
@@ -3,74 +3,81 @@
 
 using namespace std;
 
-int countPairs(bool[], int);
+int countPairs(bool taken[10]);
 void init();
+
 bool areFriends[10][10];
-int numStds;
-int numPairs;
-bool taken[10];
+int n;
 
 int main()
 {
     int numCases;
     vector<int> results;
 
-    cin>>numCases;
 
+    cin>>numCases;
     for(int i=0; i<numCases; i++){
-        cin>>numStds>>numPairs;
-        for(int j=0; j<numPairs; j++){
-            int tmp1=0, tmp2=0;
-            cin>>tmp1 >>tmp2;
-            areFriends[tmp1][tmp2]=true;
-            areFriends[tmp2][tmp1]=true;
+        
+        int numPairs=0;
+        cin>>n>>numPairs;
+
+        for(int i=0; i<numPairs; i++){
+            int tmp1;
+            int tmp2;
+            cin>>tmp1>>tmp2;
+            areFriends[tmp1][tmp2] = true;
+            areFriends[tmp2][tmp1] = true;
         }
 
-        results.push_back(countPairs(taken, 0));
+        bool taken[10] = {false};
+        
+        results.push_back(countPairs(taken));
         init();
+        
     }
 
     for(int i=0; i<numCases; i++){
         cout<<results[i]<<endl;
     }
-
     return 0;
+
 }
+
 void init()
 {
-    for(int i=0; i<numStds;i++){
-        for(int j=0; j<numStds; j++){
+    for(int i=0; i<n;i++){
+        for(int j=0; j<n; j++){
             areFriends[i][j]=false;
             areFriends[j][i]=false;
         }
     }
 }
 
-//중복으로 세지는 경우를 대비해서 특정 형태의 답만이 나오게 해야 한다.
-//least와 반복문의 j=i+1로 해결할 수 있다.
-//아니면 BASE CASE를 찾을때 어차피 한번 서치를 하게 되니 그때 Least를 구해도 괜찮다.
-int countPairs(bool taken[10], int least)
+int countPairs(bool taken[10])
 {
-    //BASE CASE
-    int finished = true;
-    for(int i=0; i<numStds; i++) if(!taken[i]) finished = false;
-    if(finished) return 1;
-
-    //끝난경우, 끝나지 않은경우로 나눠서... 끝나지 않은경우엔?
-
-    int count=0;
-    
-    for(int i=least; i<numStds; i++){
-        for(int j= i+1; j<numStds; j++){
-            if(areFriends[i][j] && !taken[i] && !taken[j]){
-                taken[i] = taken[j] = true;
-                count+= countPairs(taken, i);
-                
-                //다른 세계관
-                taken[i] = taken[j] = false;
-            }
+    int first = -1;
+    for(int i=0; i<n; i++){
+        if(!taken[i]){
+            first = i;
+            break;
         }
     }
-    return count;
+
+    if(first == -1) return 1;
+
+    int ret = 0;
+
+    for(int i = first+1; i<n; i++){
+        if(areFriends[first][i] && !taken[i]){
+            taken[first] = true;
+            taken[i] = true;
+            cout<<first<<i<<endl;
+            ret += countPairs(taken);
+            taken[first] = false;
+            taken[i] =false;        
+        }
+    }
+
+    return ret;
 }
-    
+
--- 알고리즘문제해결전략/test.cpp
+++ 알고리즘문제해결전략/test.cpp
@@ -1,96 +1,83 @@
 #include <iostream>
-#include <string>
 #include <vector>
 
 using namespace std;
 
+int countPairs(bool taken[10]);
+void init();
 
-string findWord(string word);
-bool inRange(int y, int x);
-bool hasWord(int y, int x, const string& word);
-
-
-char map[5][5];
-
-const int dy[8] = {0,  0, 1, 1, 1, -1,-1, -1};
-const int dx[8] = {1, -1, 0, 1, -1,  0, 1, -1};
-
+bool areFriends[10][10];
+int n;
 
 int main()
 {
     int numCases;
+    vector<int> results;
+
 
     cin>>numCases;
+    for(int i=0; i<numCases; i++){
+        
+        int numPairs=0;
+        cin>>n>>numPairs;
+
+        for(int i=0; i<numPairs; i++){
+            int tmp1;
+            int tmp2;
+            cin>>tmp1>>tmp2;
+            areFriends[tmp1][tmp2] = true;
+            areFriends[tmp2][tmp1] = true;
+        }
+
+        bool taken[10] = {false};
+        
+        results.push_back(countPairs(taken));
+        init();
+        
+    }
 
     for(int i=0; i<numCases; i++){
-
-        for(int j=0; j<5; j++){
-            for(int k=0; k<5; k++){
-                cin>>map[j][k];
-            }
-        }
-    
-
-        int numWords =0;
-        cin>>numWords;
-        vector<string> words;
-        
-        for(int j=0; j<numWords; j++){
-            string tmp;
-            cin>>tmp;
-            words.push_back(tmp);
-        }
-
-        for(int j=0; j<numWords; j++){
-            cout<<words[j]<<" "<<findWord(words[j])<<endl;
-        }
-
+        cout<<results[i]<<endl;
     }
-
     return 0;
+
 }
 
-string findWord(string word)
+void init()
 {
-    for(int i=0; i<5; i++){
-        for(int j=0; j<5; j++){
-            if(hasWord(i, j, word)){
-                return "YES";
-            }
+    for(int i=0; i<n;i++){
+        for(int j=0; j<n; j++){
+            areFriends[i][j]=false;
+            areFriends[j][i]=false;
         }
     }
-    return "NO";
 }
 
-bool hasWord(int y, int x, const string& word)
-{   
-    if(!inRange(y,x)){
-        return false;
-    }
-    if(map[y][x] != word[0]){
-        return false;
-    }
-    if(word.length() ==1){
-        return true;
-    }
-
-    for(int i=0; i<8; i++){
-        int n_y = y+dy[i];
-        int n_x = x+dx[i];
-
-        if(hasWord(n_y, n_x, word.substr(1))){
-            return true;
+int countPairs(bool taken[10])
+{
+    int first = -1;
+    for(int i=0; i<n; i++){
+        if(!taken[i]){
+            first = i;
+            break;
         }
     }
 
-    return false;
-    
+    if(first == -1) return 1;
+
+    int ret = 0;
+
+    for(int i = first+1; i<n; i++){
+        if(areFriends[first][i] && !taken[i]){
+            taken[first] = true;
+            taken[i] = true;
+            cout<<first<<i<<endl;
+            ret += countPairs(taken);
+            taken[first] = false;
+            taken[i] =false;        
+        }
+    }
+
+    return ret;
 }
 
-bool inRange(int y, int x)
-{
-    if(x>=0 && y>=0 && x<5 && y<5){
-        return true;
-    }
-    else return false;
-}
