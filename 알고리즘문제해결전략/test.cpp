69f697dc704842ea73cfd4b6376ee11248fac087
1T6 <qpdlf8@gmail.com>
2022-06-27 19:10:15 +0900

    asdf

--- 알고리즘문제해결전략/test.cpp
+++ 알고리즘문제해결전략/test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -9,22 +9,34 @@ string flip(string::iterator& it);
 int main()
 {
     int numCases;
-    cin>>numCases;
+    cin>> numCases;
     vector<string> results;
-
     for(int i=0; i<numCases; i++){
         string str;
         cin>>str;
         string::iterator it = str.begin();
+
         results.push_back(flip(it));
     }
+
     for(int i=0; i<numCases; i++){
         cout<<results[i]<<endl;
     }
+
     return 0;
 }
 
 string flip(string::iterator& it)
 {
+    char target = *it;
+    it++;
+    if(target == 'w' || target == 'b') 
+    return string(1, target);
+
+    string upperLeft = flip(it);
+    string upperRight = flip(it);
+    string lowerLeft = flip(it);
+    string lowerLeft = flip(it);
+
     
 }
