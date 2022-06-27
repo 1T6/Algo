802fb2ba144b9e872c1aab5fd4372d5339dfead0
1T6 <qpdlf8@gmail.com>
2022-06-27 21:42:25 +0900

    asdf

--- 알고리즘문제해결전략/quadtree.cpp
+++ 알고리즘문제해결전략/quadtree.cpp
@@ -4,43 +4,36 @@
 
 using namespace std;
 
-string reverse(string::iterator& it);
+string flip(string::iterator& it);
 
 int main()
 {
-    int numCases;
-    
-    vector<string> results;
-
-
+    int numCases=0;
     cin>>numCases;
-
+    vector<string> results;
     for(int i=0; i<numCases; i++){
-        string s;
-        cin >> s;
-        string::iterator it = s.begin();
-        results.push_back(reverse(it));
+        string str;
+        cin>>str;
+        string::iterator it = str.begin();
+        results.push_back(flip(it));
     }
-
     for(int i=0; i<numCases; i++){
         cout<<results[i]<<endl;
     }
-        
     return 0;
 }
-
-
-string reverse(string::iterator& it)
-{   
-    char target = *it;
+string flip(string::iterator& it)
+{
+    char head = *it;
     it++;
     //BASE CASE
-    if(target == 'w' || target=='b') return string(1, target);
+    if(head == 'w' || head =='b')
+        return string(1, head);
+    
+    string upperLeft = flip(it);
+    string upperRight = flip(it);
+    string lowerLeft = flip(it);
+    string lowerRight = flip(it);
 
-    string upperLeft = reverse(it);
-    string upperRight = reverse(it);
-    string lowerLeft = reverse(it);
-    string lowerRight = reverse(it);
-
-    return string("x")+lowerLeft+lowerRight+upperLeft+upperRight;
+    return string("x") + lowerLeft + lowerRight + upperLeft + upperRight;
 }
