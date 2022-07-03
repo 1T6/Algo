f935468d1356c6f465331d4b09cc0c6b95138dfd
1T6 <qpdlf8@gmail.com>
2022-07-04 00:39:19 +0900

    asdf

--- 알고리즘문제해결전략/test.cpp
+++ 알고리즘문제해결전략/test.cpp
@@ -1,39 +1,53 @@
 #include <iostream>
 #include <vector>
 
-
 using namespace std;
 
-
-int get_size(vector<int>& fence);
-
-int N;
+int get_size(vector<int>& fence, int sIdx, int eIdx);
 
 int main()
-{
-    int numCases;
+{   
+    int numCases = 0;
     cin>>numCases;
     vector<int> results;
 
     for(int i=0; i<numCases; i++){
-        cin>>N;
+        int numFences =0;
+        cin>>numFences;
         vector<int> fence;
-        for(int j=0; j<N; j++){
-            int tmp;
+        for(int j=0; j< numFences; j++){
+            int tmp =0;
             cin>>tmp;
             fence.push_back(tmp);
         }
-        results.push_back(get_size(fence));
+        results.push_back(get_size(fence, 0, numFences));
     }
 
     for(int i=0; i<numCases; i++){
         cout<<results[i]<<endl;
     }
-
+    
     return 0;
 }
 
-int get_size(vector<int>& fence)
+int get_size(vector<int>& fence, int sIdx, int eIdx)
 {
+    //BASE CASE
+    if(eIdx-sIdx == 1) return fence[sIdx];
+
+    int halfIdx = (eIdx-sIdx)/2;
+
+    int leftSize = get_size(fence, sIdx, halfIdx);
+    int rightSize = get_size(fence, halfIdx, eIdx);
+
+    int leftHeight = leftSize/(halfIdx-sIdx);
+    int rightHeight = rightSize/(eIdx-halfIdx);
+
+    int newHeight = (leftHeight>rightHeight) ? leftHeight : rightHeight;
+    int newSize = newHeight*(eIdx-sIdx);
+
     
+    int ret = (leftSize>rightHeight) ? leftSize : rightHeight;
+    if(newSize>ret) return newSize;
+    else return ret;
 }
