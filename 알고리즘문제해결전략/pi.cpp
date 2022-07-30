6801719bd36077d1cc8f672662e13570e76d2e28
1T6 <qpdlf8@gmail.com>
2022-07-31 01:39:55 +0900

    asdf

--- 알고리즘문제해결전략/pi.cpp
+++ 알고리즘문제해결전략/pi.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <cstring>
 
 using namespace std;
 
 
+string N;
+const int INF =999999;
+int cache[10002];
 
-int seq[10000];
-int cache[10000];
+int classify(int x, int y);
+int solve(int x);
 
 int main()
 {
     int numCases;
     cin>>numCases;
     vector<int> results;
+    for(int i=0; i<numCases; i++){
+        cin>>N;
 
-    for(int i=0; i<numCases;i++){
-        string tmp;
-        cin>> tmp;
-        memset(cache, -1, cache);
-        for(int j=0; j<tmp.length(); j++){
-            seq[j] = tmp[j];
-        }
-        results.push_back(solve());
+
     }
 
     for(int i=0; i<numCases; i++){
@@ -33,7 +30,7 @@ int main()
     return 0;
 }
 
-int solve(int x)
+int classify(int a, int b);
 {
     
 }
