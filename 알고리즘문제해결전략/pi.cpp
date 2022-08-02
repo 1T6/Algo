e673031bddc9cd1149e89c08292ef3b78d68d68c
1T6 <qpdlf8@gmail.com>
2022-08-02 13:31:47 +0900

    asdf

--- 알고리즘문제해결전략/pi.cpp
+++ 알고리즘문제해결전략/pi.cpp
@@ -5,13 +5,12 @@
 
 using namespace std;
 
-
 string N;
-const int INF =999999;
-int cache[10002];
+int cache[10000 ];
+int INF =999999999;
 
-int classify(int x, int y);
-int memorize(int x);
+int classify(int start, int num);
+int solve(int x);
 
 int main()
 {
@@ -20,8 +19,8 @@ int main()
     vector<int> results;
     for(int i=0; i<numCases; i++){
         memset(cache, -1, sizeof(cache));
-        cin>>N;
-        results.push_back(memorize(0));
+        cin>> N;
+        results.push_back(solve(0));
     }
 
     for(int i=0; i<numCases; i++){
@@ -30,38 +29,39 @@ int main()
 
     return 0;
 }
-
-int classify(int x, int y)
+// x부터 y개의 숫자
+int classify(int start, int num)
 {
-    string M = N.substr(x, y-x+1);
-    if(M == string(M.size(), M[0])) return 1;
-
+    string M = N.substr(start, num);
+    if(M == string(num, M[0])) return 1;
     bool progressive = true;
-    for(int i=0; i<M.size()-1; i++){
-        if(M[i+1]-M[i] != M[1]-M[0]) 
+    for(int i=1; i<M.size()-1; i++){
+        if(M[i+1]-M[i] != M[1] - M[0])
             progressive = false;
     }
-    if(progressive && abs(M[1]-M[0])==1) return 2;
+    if(progressive && abs(M[1]-M[0] == 1)) return 2;
     bool alternating = true;
     for(int i=0; i<M.size(); i++){
-        if(M[i] != M[i%2])
-            alternating = true;
+        if(M[i]!= M[i%2])
+            alternating=false;
     }
     if(alternating) return 4;
-    if(progressive) return 5; 
+    if(progressive) return 5;
     return 10;
 }
-
-int memorize(int x)
+int solve(int start)
 {
-    if(x==N.size()) return 0;
-    
-    int& ret = cache[x];
-    if(ret!= -1) return ret;
+    if(start == N.size()-3) return 0;
+    int& ret = cache[start];
+    if(ret != -1) return ret;
+
     ret = INF;
-    for(int i = 3; i<=5; i++){
-        if(x+i <= N.size())
-            ret = min(ret, memorize(x+i)+classify(x, x+i-1));
+
+    for(int chunk=3; chunk<=5; chunk++){
+        if(start+chunk < N.size())
+            ret = min(ret, solve(start+chunk) + classify(start, chunk));
     }
+
     return ret;
 }
+
--- 알고리즘문제해결전략/test.cpp
+++ 알고리즘문제해결전략/test.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 #include <vector>
-#include <string>
 
 using namespace std;
 
+
+
+
 int main()
 {
-    string a ="sadf";
-    string k = string(a.size(), a[0]);
-    cout<<k<<endl;
-    
-    return 0;
+    int a[10];
+    cout<<a[11]<<endl;
 }
-
-
