68f29d0806834ee0703ce6baae26d551b7e671ff
1T6 <qpdlf8@gmail.com>
2022-08-03 00:04:50 +0900

    asdf

---
+++ 알고리즘문제해결전략/quantization.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+int S;
+int seq[100];
+
+int solve();
+int sse();
+
+int main()
+{
+    int numCases;
+    cin>>numCases;
+    vector<int> results;
+    for(int i=0; i<numCases; i++){
+        cin>>N>>S;
+        for(int j=0; j<N; j++){
+            cin>>seq[j];
+        }
+        results.push_back(solve());
+    }
+    for(int i=0; i<numCases; i++){
+        cout<<results[i]<<endl;
+    }
+    return 0;
+}
+
+int solve()
+{
+
+}
+int sse()
+{
+
+}
+
