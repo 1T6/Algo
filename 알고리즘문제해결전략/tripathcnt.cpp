736d924585b55daa1db7a4210073a55fbd352fc2
1T6 <qpdlf8@gmail.com>
2022-08-04 13:40:18 +0900

    asdf

--- 알고리즘문제해결전략/tripathcnt.cpp
+++ 알고리즘문제해결전략/tripathcnt.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
+int n;
+int seq[5050];
+
+int solve();
+
 int main()
-{
+{   
     int numCases;
     cin>>numCases;
     vector<int> results;
-    
+    for(int i=0; i<numCases; i++){
+        cin>> n;
+        for(int j=0; j < n*(n+1)/2; j++){
+            cin>>seq[j];
+        }
+        results.push_back(solve());
+    }
+    for(int i=0; i<numCases; i++){
+        cout<<results[i]<<endl;
+    }
+
     return 0;
 }
+
