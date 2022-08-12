9907e27726b4182b07bf26049e3f6d8af14907ce
1T6 <qpdlf8@gmail.com>
2022-08-12 23:08:49 +0900

    asdf

--- 알고리즘문제해결전략/poly.cpp
+++ 알고리즘문제해결전략/poly.cpp
@@ -4,11 +4,12 @@
 
 using namespace std;
 
-
 int n;
 int solve();
 int mod = 10000000;
-
+int cache[100][100];
+int cnt(int rest, int first);
+int solve();
 int main()
 {
 
@@ -24,5 +25,23 @@ int main()
      }
      return 0;
 }
-
-
+int cnt(int rest, int first)
+{
+     if(rest == first) return 1;
+     int& ret = cache[rest][first];
+     if(ret!= -1) return ret;
+     ret =0;
+     for(int i=1; i<=rest-first; i++){
+          ret +=cnt(rest-first, i)*(first+i-1)/mod;
+     }
+     return ret;
+}
+int solve()
+{
+     memset(cache, -1, sizeof(cache));
+     int ret =0;
+     for(int i=1; i<=n; i++){
+          ret+=cnt(n,i);
+     }
+     return ret;
+}
