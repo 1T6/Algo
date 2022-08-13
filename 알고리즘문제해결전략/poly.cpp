71d63aaec902529e0d6c80a9c52406baf74a243a
1T6 <qpdlf8@gmail.com>
2022-08-13 13:38:51 +0900

    asdf

---
+++ 알고리즘문제해결전략/poly.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+
+int main()
+{
+     int numCases;
+     cin>>numCases;
+     vector<int> results;
+     for(int i=0; i<numCases; i++){
+          
+     }
+     return 0;
+}
+
