5a381fcb70039900111254c4795c497904fdac31
1T6 <qpdlf8@gmail.com>
2022-08-15 12:48:00 +0900

    asdf

--- 알고리즘문제해결전략/test.cpp
+++ 알고리즘문제해결전략/test.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
-
+#include <typeinfo>
 using namespace std;
 
 int main()
 {
+     int a[] = {1,2,3,4,5,6,7};
 
-     vector<vector<int>> a;
-     return 0;
+     for(auto number : a){
+          cout<<number<<endl; 
+     }
 }
