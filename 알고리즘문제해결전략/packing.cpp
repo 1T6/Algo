ac8699f8727505cbe7509c179de6000527042b28
1T6 <qpdlf8@gmail.com>
2022-08-16 02:39:40 +0900

    asdf

--- 알고리즘문제해결전략/packing.cpp
+++ 알고리즘문제해결전략/packing.cpp
@@ -1,2 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int numCases;
+    cin>>numCases;
+    for(int i=0; i<numCases; i++){
+        
+    }
+    return 0;
+}
 
 
+
+int pack(int rest, int item)
+{   
+    rest -= need[item];
+    ret =0;
+    best = -1;
+    for(int next=rest+1; next<n; next++){
+        if(rest > need[next]){
+            int tmp = pack(rest, next);
+            if(tmp > ret){
+                best = next;
+                ret = tmp;
+            }
+        }
+    }
+    choice[item] = best;
+    return ret;
+}
