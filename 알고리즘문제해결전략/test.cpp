4df3d7990600c8c2c2019cf7dfc94cd1377342c7
1T6 <qpdlf8@gmail.com>
2022-08-16 19:54:15 +0900

    asdf

--- 알고리즘문제해결전략/test.cpp
+++ 알고리즘문제해결전략/test.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <vector>
-#include <typeinfo>
+
+
 using namespace std;
+
+int a[50];
 
 int main()
 {
-     int a[] = {1,2,3,4,5,6,7};
-
-     for(auto number : a){
-          cout<<number<<endl; 
-     }
+     memset(a, -1, sizeof(a));
+     return 0;
 }
