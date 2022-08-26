ae28fcd42d0ba64849e1a7cecc611d104a9e6a6e
1T6 <qpdlf8@gmail.com>
2022-08-26 15:55:10 +0900

    asdf

--- 알고리즘문제해결전략/ocr.cpp
+++ 알고리즘문제해결전략/ocr.cpp
@@ -1,124 +1,109 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <map>
-#include <cmath>
+#include <string>
 #include <limits>
+#include <cmath>
+#include <map>
+#include <algorithm>
+#include <cstring>
 
 using namespace std;
 
 int m, q;
+map<string,int> strToIdx;
+string corpus[500];
 
-map<string, int> strToIdx;
-map<int, string> idxToStr;
-
-
-double B[500];
-double T[500][500];
-double M[500][500];
-
+double T[501][500], M[500][500];
 
 int n;
+int words[100];
 
-string words[100];
-
-double cache[100][500];
-
-int choice[100][500];
+double cache[100][501];
+int choice[100][501];
 
 
-
-void solve();
 double maxProb(int target, int before);
 string getString(int target, int before);
-
+void solve();
 
 int main()
-{
-    cin>>m>>q;
-
-    for(int i=0; i<m; i++){
-        string tmp;
-        cin>>tmp;
-        strToIdx.insert(pair<string, int>(tmp, i));
-        idxToStr.insert(pair<int, string>(i, tmp));
-    }
-    for(int i=0; i<m; i++){
-        double tmp;
-        cin>>tmp;
-        tmp = log(tmp);
-        B[i] = tmp;
-    }
-    for(int i=0; i<m; i++){
-        for(int j=0; j<m; j++){
-                double tmp;
-                cin>>tmp;
-                tmp = log(tmp);
-                T[i][j] = tmp;
-        }
-    }
-    for(int i=0; i<m; i++){
-        for(int j=0; j<m; j++){
-                double tmp;
-                cin>>tmp;
-                tmp = log(tmp);
-                M[i][j] = tmp;
-        }
-    }
-
-    for(int i=0; i<q; i++){
-          cin>>n;
-          for(int j=0; j<n; j++){
-               cin>>words[j];
-          }
-          solve();
-    }
-
-    return 0;
+{	
+	
+	
+	cin>>m>>q;
+	for(int i=0; i<m; i++){
+		string tmp;
+		cin>>tmp;
+		strToIdx.insert(pair<string, int>(tmp, i));
+		corpus[i] = tmp;
+	}
+	for(int i=0; i<m; i++){
+		double tmp;
+		cin>>tmp;
+		T[0][i] = log(tmp);
+	}
+	for(int i=0; i<m; i++){
+		for(int j=0; j<m; j++){
+			double tmp;
+			cin>>tmp;
+			T[i+1][j] = log(tmp);
+		}
+	}
+	for(int i=0; i<m; i++){
+		for(int j=0; j<m; j++){
+			double tmp;
+			cin>>tmp;
+			M[i][j] = log(tmp);
+		}
+	}
+	for(int i=0; i<q; i++){
+		cin>>n;
+		for(int j=0; j<n; j++){
+			string tmp;
+			cin>>tmp;
+			words[j] = strToIdx[tmp];
+		}
+		solve();
+	}
+	
+	return 0;
 }
-
 double maxProb(int target, int before)
 {
-     if(target == n) return 0;
-     double& ret = cache[target][before];
-     if(ret > -0.5) return ret;
+	if(target == n) return 0;
+	double& ret = cache[target][before];
+	if(ret > -0.5) return ret;
 
-     ret = numeric_limits<double>::lowest();
-     
-     int& choose = choice[target][before];
+	ret = -1e200;
 
-     for(int i=0; i<m ; i++){
-          double cand;
-          if(target ==0){
-               cand = B[i] + M[i][strToIdx[words[target]]] + maxProb(target+1, i);
-          }
-          else{
-               cand = T[before][i] + M[i][strToIdx[words[target]]] + maxProb(target+1, i);
-          }
-          if(cand > ret){
-               ret = cand;
-               choose = i;    
-          }
-     }
-     return ret;
+	int& choose = choice[target][before];
+
+	for(int i=0; i<m; i++){
+		
+		double cand = T[before+1][i] + M[i][words[target]] + maxProb(target+1, i);
+		if(cand>ret){
+			ret = cand;
+			choose = i;
+		}
+	}
+	return ret;
+}
+
+string getString(int target, int before)
+{
+	int choose = choice[target][before];
+	string ret = corpus[choose];
+	if(target < n-1)
+		ret +=" " + getString(target+1, choose);
+	return ret;
 }
 
 void solve()
 {
-     fill(&cache[0][0], & cache[99][500], -1);
-     double tmp = maxProb(0,0);
+	fill(&cache[0][0], &cache[99][501],  -1);
+	memset(choice, -1, sizeof(choice));
+	double res = maxProb(0,0);
+	string ret = getString(0,0);
 
-     string ret = getString(0,0);
-     cout<<ret<<endl;
-     
+	cout<<ret<<endl;
 }
-string getString(int target, int before)
-{
-     int choose = choice[target][before];
-     string ret = idxToStr[choose];
-     if(target<n-1){
-          ret  += " " + getString(target+1, choose);
-     }
-     return ret;
-}
-
