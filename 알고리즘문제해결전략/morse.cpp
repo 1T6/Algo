5a59c18fe297c6e1a9095ad1ef23f269cb656797
1T6 <qpdlf8@gmail.com>
2022-08-28 19:29:42 +0900

    asdf

--- 알고리즘문제해결전략/morse.cpp
+++ 알고리즘문제해결전략/morse.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
 #include <vector>
 
-
 using namespace std;
 
-int nCr(int n, int r);
+int n,m,k;
+
 int fact(int n);
+int nCr(int n, int r);
 
+int main()
+{
+    /*
+    int numCases;
+    cin>>numCases;
+    for(int i=0; i<numCases; i++){
+        cin>>n>>m>>k;
+        solve();
+    }
+    */
 
+    return 0;
+}
+
+int fact(int n)
+{
+    int ret =1;
+    while(n>1){
+        ret*=n;
+        n--;
+    }
+    return ret;
+}
+
+int nCr(int n, int r)
+{
+    return fact(n)/(fact(r)*fact(n-r));
+}
+
+string kth(int n, int m, int skip)
+{
+
+}
