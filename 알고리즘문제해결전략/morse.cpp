310ba17810e902b6ccb34242eb35d916e72056cf
1T6 <qpdlf8@gmail.com>
2022-08-29 11:17:14 +0900

    asdf

--- 알고리즘문제해결전략/morse.cpp
+++ 알고리즘문제해결전략/morse.cpp
@@ -1,52 +1,48 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
+int bino[201][201];
+
+void calcBino();
+string kth(int n,int m,int k);
 
 
-int fact(int n);
-int nCr(int n, int r);
+int M = 1000000000+10;
 
 int main()
 {
-    
     int numCases;
     cin>>numCases;
+    calcBino();
     for(int i=0; i<numCases; i++){
-        int n=0, m=0, k=0;
+        int n, m, k;
         cin>>n>>m>>k;
         string result = kth(n, m, k);
         cout<<result<<endl;
     }
-    
 
     return 0;
 }
 
-int fact(int n)
+void calcBino()
 {
-    int ret =1;
-    while(n>1){
-        ret*=n;
-        n--;
+    memset(bino, 0, sizeof(bino));
+    for(int i=0; i<=200; i++){
+        bino[i][0] = bino[i][i] = 1;
+        for(int j=1; j<i; j++){
+            bino[i][j] = min(M, bino[i-1][j-1] + bino[i-1][j]);
+        }
     }
-    return ret;
-}
-
-int nCr(int n, int r)
-{
-    return fact(n)/(fact(r)*fact(n-r));
 }
 
 string kth(int n, int m, int remain)
 {
-    
+    if(n==0) return string(m, 'o');
 
-    if(remain < nCr(n+m-1, n-1))
+    if(remain <= bino[n+m-1][n-1])
         return "-" + kth(n-1, m, remain);
-    return "o" + kth(n, m-1, remain - nCr(n+m-1, n-1));
-
-    
-
+    return "o" + kth(n, m-1, remain - bino[n+m-1][n-1]);
 }
--- 알고리즘문제해결전략/test.cpp
+++ 알고리즘문제해결전략/test.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+
 
 using namespace std;
 
+int fact(int n);
+int nCr(int n, int r);
 
 int main()
-{
+{   
+    cout<<fact(100);
+    
 
-    cout<<nCr(5,3);
     return 0;
 }
 
+int fact(int n)
+{
+    int ret =1;
+    while(n>1){
+        ret*=n;
+        n--;
+    }
+    return ret;
+}
+int nCr(int n, int r)
+{
+    return fact(n)/(fact(r)*fact(n-r));
+}
